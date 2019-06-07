a0f5e5a4616740b35f3f63383382aa0d94258d7b
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2019-06-07 20:39:41 +0700

    :sparkles: Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+import { getInitialFlightData } from "./DataProvider";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: getInitialFlightData()
+    };
+    
+    this.columns = [
+      {
+        Header: "Origin",
+        accessor: "origin"
+      },
+      {
+        Header: "Flight",
+        accessor: "flight"
+      },
+      {
+        Header: "Arrival",
+        accessor: "arrival"
+      },
+      {
+        Header: "State",
+        accessor: "state"
+      }
+    ];
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ReactTable data={this.state.data} columns={this.columns} />
+      </div>
+    );
+  }
 }
 
 export default App;
